b9bbad3485f8ccc2ae39e75cb06d429c059c4b85
jean paul <jeanpaulabizeid@gmail.com>
2022-07-03 19:13:21 +0300

    backend: creating the users table

--- SOS-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ SOS-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('role_id');
+            $table->string('picture')->nullable();;
+            $table->string('dob')->nullable();;
+            $table->string('number')->nullable();;
+            $table->string('preffere_contact')->nullable();;
+            $table->string('address')->nullable();;
+            $table->string('gender')->nullable();;
             $table->timestamps();
         });
     }
