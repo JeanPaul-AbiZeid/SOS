3a0d94247a2dd46598ad6c360fbd8c23f8e367e5
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 18:26:01 +0300

    frontend: creating Login component

---
+++ SOS-ReactNative/components/Login/Login.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+export default function LogIn() {
+  return (
+    <View style={styles.container}>
+      <Text>SOS</Text>
+    </View>
+  );
+}
