aab6ce4160e7ba1589907c8b9ece7c9e62ee7a86
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 21:26:00 +0300

    frontend: implementing useState to get the value of the email and password

--- SOS-ReactNative/components/Login/Login.js
+++ SOS-ReactNative/components/Login/Login.js
@@ -1,14 +1,23 @@
 import { Button, Pressable, Text, TextInput, View } from 'react-native';
+import React from "react";
 import { Checkbox } from 'react-native-paper';
 import styles from './styles';
 
 export default function LogIn() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
   return (
     <View style={styles.container}>
       <Text style={styles.title}>SOS</Text>
       <Text style={styles.head}>Sign In</Text>
-      <TextInput placeholder='Email' style={styles.input}/>
-      <TextInput placeholder='Password' style={styles.input}/>
+      <TextInput 
+      placeholder='Email' 
+      style={styles.input}
+      onChangeText={newText => setEmail(newText)}/>
+      <TextInput 
+      placeholder='Password' 
+      style={styles.input}
+      onChangeText={newText => setPassword(newText)}/>
       <View style={styles.pass}>
         <Checkbox />
         <Text >Show Password</Text></View>
