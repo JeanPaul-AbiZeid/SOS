9c2d7e01afa771d0ee869b0d37a7e358d8874c7a
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 21:53:10 +0300

    frontend: creating toggle hook and removing the checkbox from the log in page

--- SOS-ReactNative/components/Login/Login.js
+++ SOS-ReactNative/components/Login/Login.js
@@ -1,11 +1,11 @@
-import { Button, Pressable, Text, TextInput, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 import React from "react";
-import { Checkbox } from 'react-native-paper';
 import styles from './styles';
 
 export default function LogIn() {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>SOS</Text>
@@ -18,9 +18,6 @@ export default function LogIn() {
       placeholder='Password' 
       style={styles.input}
       onChangeText={newText => setPassword(newText)}/>
-      <View style={styles.pass}>
-        <Checkbox />
-        <Text >Show Password</Text></View>
       <Pressable style={styles.button}>
         <Text style={styles.btnText}>Sign in</Text>
         </Pressable>
