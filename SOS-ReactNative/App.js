28d6d4278e78f704e7fd3e892bfd641e70ccaccb
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 19:00:49 +0300

    frontend: creating sign up component

--- SOS-ReactNative/App.js
+++ SOS-ReactNative/App.js
@@ -1,8 +1,9 @@
 import LogIn from './components/Login/Login';
+import Signup from './components/Sign up/Signup';
 
 export default function App() {
   return (
-    <LogIn/>
+    <Signup/>
   );
 }
 
---
+++ SOS-ReactNative/components/Sign up/Signup.js
@@ -0,0 +1,10 @@
+import { Button, Text, TextInput, View } from 'react-native';
+import styles from './styles';
+
+export default function Signup() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
