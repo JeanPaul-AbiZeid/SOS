14575a9e71db8d3c1b38c888a8f40a12af6b23c6
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 19:04:28 +0300

    frontend: filling the sign up page with titles and input fields + the sign up button

--- SOS-ReactNative/components/Sign up/Signup.js
+++ SOS-ReactNative/components/Sign up/Signup.js
@@ -4,7 +4,13 @@ import styles from './styles';
 export default function Signup() {
   return (
     <View style={styles.container}>
-      
+      <Text>SOS</Text>
+      <Text>Sign Up</Text>
+      <TextInput placeholder='First Name'/>
+      <TextInput placeholder='Last Name'/>
+      <TextInput placeholder='Email'/>
+      <TextInput placeholder='Password'/>
+      <Button title="Sign Up" color="red"/>
     </View>
   );
 }
