d2e6a418b1bfd4e9c8fc5732c86216825dfeb532
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 19:29:12 +0300

    frontend: installing select picker + adding dropdowns for blood type and gender in the sign up page

--- SOS-ReactNative/components/Sign up/Signup.js
+++ SOS-ReactNative/components/Sign up/Signup.js
@@ -1,4 +1,5 @@
 import { Button, Text, TextInput, View } from 'react-native';
+import RNPickerSelect from "react-native-picker-select";
 import styles from './styles';
 
 export default function Signup() {
@@ -10,6 +11,29 @@ export default function Signup() {
       <TextInput placeholder='Last Name'/>
       <TextInput placeholder='Email'/>
       <TextInput placeholder='Password'/>
+      <Text>Blood type</Text>
+      <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: "O+", value: "O+" },
+                { label: "O-", value: "O-" },
+                { label: "A+", value: "A+" },
+                { label: "A-", value: "A-" },
+                { label: "B+", value: "B+" },
+                { label: "B-", value: "B-" },
+                { label: "AB+", value: "AB+" },
+                { label: "AB-", value: "AB-" },
+            ]}
+        />
+        <Text>Gender</Text>
+        <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: "Male", value: "Male" },
+                { label: "Female", value: "Female" },
+                { label: "Other", value: "Other" },
+            ]}
+        />
       <Button title="Sign Up" color="red"/>
     </View>
   );
