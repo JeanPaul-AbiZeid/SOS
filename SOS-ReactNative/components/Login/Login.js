f408c5d53c226387252bee3180e5858b0d55c7d7
jean paul <jeanpaulabizeid@gmail.com>
2022-07-08 18:33:34 +0300

    frontend: placing the texts and input fields in the log in page

--- SOS-ReactNative/components/Login/Login.js
+++ SOS-ReactNative/components/Login/Login.js
@@ -1,10 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import styles from './styles';
 
 export default function LogIn() {
   return (
     <View style={styles.container}>
       <Text>SOS</Text>
+      <Text>Sign In</Text>
+      <TextInput placeholder='Email'/>
+      <TextInput placeholder='Password'/>
     </View>
   );
 }
