d3cf05d62acc1dd80c89da66b3844516637410ef
jean paul <jeanpaulabizeid@gmail.com>
2022-07-13 11:20:08 +0300

    frontend: creating the CreateAlert folder + component

---
+++ SOS-ReactNative/components/CreateAlert/CreateAlert.js
@@ -0,0 +1,8 @@
+import { Pressable, ScrollView, Text, TextInput, View } from 'react-native';
+import React from "react";
+import styles from './styles';
+
+export default function CreateAlert() {
+
+
+}
