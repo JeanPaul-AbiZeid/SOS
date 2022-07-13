1a5ce20ca4d7fbe2864aec0a77f5bba9d56fdf64
jean paul <jeanpaulabizeid@gmail.com>
2022-07-13 10:49:05 +0300

    frontend: inserting images to the home page

--- SOS-ReactNative/components/Home/Home.js
+++ SOS-ReactNative/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, ScrollView, Text, TextInput, View } from 'react-native';
+import { Pressable, ScrollView, Text, TextInput, View, Image } from 'react-native';
 import styles from './styles';
 
 
@@ -8,7 +8,10 @@ export default function Home() {
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.title}>SOS</Text>
-            
+            <Image source={require('../../assets/police-logo.png')}/>
+            <Image source={require('../../assets/ambulance-logo.png')}/>
+            <Image source={require('../../assets/fire-logo.png')}/>
+            <Image source={require('../../assets/contact-logo.png')}/>
         </ScrollView>
     )
     
