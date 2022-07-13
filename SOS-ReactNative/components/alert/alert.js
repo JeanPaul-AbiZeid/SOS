d135e896546167a32b938f4bc35584569acf6a75
jean paul <jeanpaulabizeid@gmail.com>
2022-07-13 12:31:31 +0300

    frontend: adding the content to the alert component

--- SOS-ReactNative/components/alert/alert.js
+++ SOS-ReactNative/components/alert/alert.js
@@ -5,7 +5,12 @@ import styles from './styles';
 export default function alert() {
 
   return (
-    <View >
+    <View style={styles.main}>
+        <Image style={styles.img} source={require('../../assets/persona.png')}/>
+        <View>
+            <Text style={styles.name}>John Doe</Text>
+            <Text style={styles.desc}>Notification</Text>
+        </View>
     </View>
   );
 }
--- SOS-ReactNative/components/alert/styles.js
+++ SOS-ReactNative/components/alert/styles.js
@@ -1,7 +1,22 @@
 import { StyleSheet, StatusBar } from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    main: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: '200'
+    },
+    desc: {
+        fontSize: 18
+    },
+    img: {
+        width: 50,
+        height: 50,
+        borderRadius: 60
+    }
 });
 
 export default styles;
