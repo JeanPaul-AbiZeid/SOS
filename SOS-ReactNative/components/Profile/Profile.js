df41c5bca11937abc01d5c486ff4a1a6dd587e9d
jean paul <jeanpaulabizeid@gmail.com>
2022-07-14 13:03:24 +0300

    frontend: creating the profile page component

---
+++ SOS-ReactNative/components/Profile/Profile.js
@@ -0,0 +1,10 @@
+import { Text, View, Image, ScrollView } from 'react-native';
+import React from "react";
+import styles from './styles';
+
+export default function Profile() {
+
+    return (
+      
+    );
+  }
