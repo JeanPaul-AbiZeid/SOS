416e24adc091c7a636058592de0361d460dd2c8a
jean paul <jeanpaulabizeid@gmail.com>
2022-07-15 15:27:28 +0300

    frontend: adding navigation for sign up pages

--- SOS-ReactNative/components/SignUp/SignUp.js
+++ SOS-ReactNative/components/SignUp/SignUp.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 
-export default function SignUp() {
+export default function SignUp({navigation}) {
     
   return (
     <View style={styles.container}>
         <Text style={styles.title}>SOS</Text>      
-    
-        <Pressable style={styles.button}>
-            <Text style={styles.btnText}>Sign Up User</Text>
-        </Pressable>
 
-        <Pressable style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.push('SignUpUser')}>
+            <Text style={styles.btnText}>Sign Up User</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.push('SignUpExpert')}>
             <Text style={styles.btnText}>Sign Up Expert</Text>
-        </Pressable>
+        </TouchableOpacity>
+      
     </View>
   );
 }
