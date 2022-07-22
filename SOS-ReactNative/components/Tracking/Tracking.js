dfbccda0ffe7638c02863528fed20bad3d689aba
jean paul <jeanpaulabizeid@gmail.com>
2022-07-22 10:47:07 +0300

    frontend: showing the google map inside the container in the tracking page:

--- SOS-ReactNative/components/Tracking/Tracking.js
+++ SOS-ReactNative/components/Tracking/Tracking.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {  Text, View } from 'react-native';
+import {  Text, View, StyleSheet } from 'react-native';
 import styles from './styles';
+import MapView, { Marker, AnimatedRegion } from 'react-native-maps';
 
 export default function Tracking() {
+  const mapRef = React.useRef()
+  const markerRef = React.useRef()
     
   return (
     <View style={styles.container}>
         <Text style={styles.title}>Tracking</Text>
-        <View style={styles.map}></View>
+        <View style={styles.map}>
+        <MapView
+                    ref={mapRef}
+                    style={StyleSheet.absoluteFill}
+                    
+                >
+
+                    
+
+                    
+
+                    
+                </MapView>
+        </View>
     </View>
   );
 }
