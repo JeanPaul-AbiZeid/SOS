344e41ade260fe5d31e7c2add24238332001e2ca
jean paul <jeanpaulabizeid@gmail.com>
2022-07-23 20:57:11 +0300

    frontned: importing firebase and configuration to create alert component + creating a function to send a notification to all the users

--- SOS-ReactNative/components/CreateAlert/CreateAlert.js
+++ SOS-ReactNative/components/CreateAlert/CreateAlert.js
@@ -3,7 +3,24 @@ import React from "react";
 import styles from './styles';
 import * as ImagePicker from 'expo-image-picker';
 import * as Notifications from 'expo-notifications';
-import {useUserInfo} from '../../hooks/UserProvider'
+import {useUserInfo} from '../../hooks/UserProvider';
+import { getFirestore, collection, doc, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAD4M-WQhjPkCaWfhRSIRmPfsHWReItWxw",
+  authDomain: "sosapp-877db.firebaseapp.com",
+  projectId: "sosapp-877db",
+  storageBucket: "sosapp-877db.appspot.com",
+  messagingSenderId: "552222011747",
+  appId: "1:552222011747:web:cd7cb766e463a47dd92356",
+  measurementId: "G-6SMB0X6W6Y"
+};
+
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const firestore = getFirestore(app, {experimentalForceDetectLongPolling : true});
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -78,8 +95,7 @@ export default function CreateAlert() {
 
       <TouchableOpacity style={styles.button}
       onPress={async () => {
-        await sendPushNotification(expoPushToken, user.first_name + ' ' + user.last_name, title);
-        alert(expoPushToken)
+        await sendPushNotificationToAllUsers(user.id, user.first_name + ' ' + user.last_name, title);
       }}>
           <Text style={styles.btnText}>Send Alert</Text>
       </TouchableOpacity>
@@ -107,5 +123,14 @@ async function sendPushNotification(expoPushToken, user, caseTitle) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(message),
+  });
+}
+
+const sendPushNotificationToAllUsers = async (id, userInfo, caseTitle) => {
+  const users = await getDocs(collection(firestore, "users"));
+  users.forEach((res) => {
+    if (res.id != id) {
+      sendPushNotification(res.data().token, userInfo, caseTitle)
+    }
   });
 }
